34dc2df53334a070ec5a696bed0ad55125f118f1
Vijay Bhasin <vijaybhasin2112@outlook.com>
2020-05-26 19:59:03 +0530

    Start Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import{LoginComponent} from './login/login.component';
+import{RegisterComponent} from './register/register.component';
 
 
 
 const routes: Routes = [
   {path:'',component:DashboardComponent},
   {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent}, //routing
 
   { path: '**', redirectTo: '' }
 ];
