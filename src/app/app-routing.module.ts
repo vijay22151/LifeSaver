cd0f92d2e14aa3d6047c09831a48d671dc84b595
Vijay Bhasin <vijaybhasin2112@outlook.com>
2020-05-26 19:26:02 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import{LoginComponent} from './login/login.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'login',component:LoginComponent},
+
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
