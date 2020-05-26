f779e4805cf28566f32266dfa2f4a3e918c7a2c3
Vijay Bhasin <vijaybhasin2112@outlook.com>
2020-05-26 19:41:57 +0530

    Reslove merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-=======
-import { RegisterComponent } from './register/register.component';
->>>>>>> 6fa7a71c9b0b7d63020245b539da6f7a606141b4
+import{RegisterComponent} from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
-    DashboardComponent
-=======
+    DashboardComponent,
     RegisterComponent
->>>>>>> 6fa7a71c9b0b7d63020245b539da6f7a606141b4
   ],
   imports: [
     BrowserModule,
